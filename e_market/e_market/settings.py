0cf35ef7f16fad6eb4ee7bba488d32280c51af71
caesar699 <caesar699@gmail.com>
2022-03-14 09:55:24 +0600

    Hiding SECRET_KEY

--- e_market/e_market/settings.py
+++ e_market/e_market/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--j2471p+537hmd%z*h@jb1$nk%a4dg1eyl+s3$6w7*@kdb6)y-'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
