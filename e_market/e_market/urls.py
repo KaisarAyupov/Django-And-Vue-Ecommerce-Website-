7fe3dfd6c74d1ddb069d169a328f41b0365f0f8a
caesar699 <caesar699@gmail.com>
2022-03-14 11:41:29 +0600

    Creating a contact simple page

--- e_market/e_market/urls.py
+++ e_market/e_market/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from apps.core.views import frontpage, contact
+
 urlpatterns = [
+    path('', frontpage, name='frontpage'),
+    path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
