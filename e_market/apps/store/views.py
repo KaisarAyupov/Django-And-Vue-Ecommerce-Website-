ff1cb268dca14546c9a580cc02a3377d76083808
caesar699 <caesar699@gmail.com>
2022-03-15 11:35:31 +0600

    Show detail view for a product

--- e_market/apps/store/views.py
+++ e_market/apps/store/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
-# Create your views here.
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+
+    context = {
+        'product' : product
+    }
+    
+    return render(request, 'product_detail.html', context)
--- e_market/e_market/urls.py
+++ e_market/e_market/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 
 from apps.core.views import frontpage, contact
+from apps.store.views import product_detail
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
     path('contact/', contact, name='contact'),
+    path('<slug:slug>/', product_detail, name='product_detail' ),
     path('admin/', admin.site.urls),
 ]
