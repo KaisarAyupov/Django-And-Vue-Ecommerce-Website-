d366f5bb94e426058e09f8854c75bf6175ea7577
caesar699 <caesar699@gmail.com>
2022-03-15 11:49:45 +0600

    Show categories in the menu

--- e_market/e_market/settings.py
+++ e_market/e_market/settings.py
@@ -68,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'apps.store.context_processors.menu_categories',
             ],
         },
     },
