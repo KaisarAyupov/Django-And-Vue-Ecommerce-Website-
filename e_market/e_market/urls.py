d1ef7d17d75b30d89aad13eda6af979db28fbfef
caesar699 <caesar699@gmail.com>
2022-03-16 09:55:15 +0600

    Create a view for the cart and show link in the menu

--- e_market/e_market/urls.py
+++ e_market/e_market/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from apps.cart.views import cart
 from apps.core.views import frontpage, contact, about
 from apps.store.views import product_detail, category_detail
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
+    path('cart/', cart, name='cart'),
     path('contact/', contact, name='contact'),
     path('about/', about, name='about'),
     path('admin/', admin.site.urls),
