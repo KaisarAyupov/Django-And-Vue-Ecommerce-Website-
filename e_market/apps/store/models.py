8d243bbd18bacd280bd596640505f1ba8c696e89
caesar699 <caesar699@gmail.com>
2022-03-16 09:27:29 +0600

    Make it possible to set ordering for the categories

--- e_market/apps/store/models.py
+++ e_market/apps/store/models.py
@@ -6,9 +6,11 @@ from django.db import models
 class Category(models.Model):
     title= models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
+    ordering = models.IntegerField(default=0)
 
     class Meta:
         verbose_name_plural = 'Categories'
+        ordering =('ordering',)
 
     def __str__(self):
         return self.title
@@ -19,6 +21,10 @@ class Product(models.Model):
     slug = models.SlugField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.FloatField()
+    date_added =models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering =('-date_added',)
 
     def __str__(self):
         return self.title
--- e_market/e_market/urls.py
+++ e_market/e_market/urls.py
@@ -23,7 +23,8 @@ urlpatterns = [
     path('', frontpage, name='frontpage'),
     path('contact/', contact, name='contact'),
     path('about/', about, name='about'),
+    path('admin/', admin.site.urls),
     path('<slug:category_slug>/<slug:slug>/', product_detail, name='product_detail' ),
     path('<slug:slug>/', category_detail, name='category_detail' ),
-    path('admin/', admin.site.urls),
+    
 ]
