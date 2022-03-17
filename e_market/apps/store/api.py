6fe7560dcba35e0b1fbbdff6c7975745eff200b3
caesar699 <caesar699@gmail.com>
2022-03-17 10:04:45 +0600

    Create a Cart class

---
+++ e_market/apps/store/api.py
@@ -0,0 +1,36 @@
+import json
+#import stripe
+#import razorpay
+
+#from django.conf import settings
+#from django.core.mail import EmailMultiAlternatives
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import get_object_or_404, redirect
+from django.template.loader import render_to_string
+
+#from paypalcheckoutsdk.core import PayPalHttpClient, SandboxEnvironment
+#from paypalcheckoutsdk.orders import OrdersCaptureRequest
+
+from apps.cart.cart import Cart
+#from apps.order.views import render_to_pdf
+from .models import Product
+
+
+def api_add_to_cart(request):
+    data = json.loads(request.body)
+    jsonresponse = {'success': True}
+    product_id = data['product_id']
+    update = data['update']
+    quantity = data['quantity']
+
+    cart = Cart(request)
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    if not update:
+        cart.add(product=product, quantity=1, update_quantity=False)
+    else:
+        cart.add(product=product, quantity=quantity, update_quantity=True)
+    
+    return JsonResponse(jsonresponse)
+
--- e_market/e_market/urls.py
+++ e_market/e_market/urls.py
@@ -20,12 +20,22 @@ from apps.cart.views import cart_detail
 from apps.core.views import frontpage, contact, about
 from apps.store.views import product_detail, category_detail
 
+from apps.store.api import api_add_to_cart
+
 urlpatterns = [
     path('', frontpage, name='frontpage'),
     path('cart/', cart_detail, name='cart'),
     path('contact/', contact, name='contact'),
     path('about/', about, name='about'),
     path('admin/', admin.site.urls),
+
+    # API
+
+    path('api/api_add_to_cart/', api_add_to_cart, name='api_add_to_cart'),
+
+    # Store
+
+
     path('<slug:category_slug>/<slug:slug>/', product_detail, name='product_detail' ),
     path('<slug:slug>/', category_detail, name='category_detail' ),
     
