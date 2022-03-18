1256652666b757cdba68bb7b0a7028218b70475a
caesar699 <caesar699@gmail.com>
2022-03-18 14:29:45 +0600

    Error

--- e_market/apps/cart/views.py
+++ e_market/apps/cart/views.py
@@ -1,3 +1,4 @@
+from itertools import product
 from django.conf import settings
 from django.shortcuts import render, redirect
 
@@ -5,10 +6,18 @@ from .cart import Cart
 
 
 def cart_detail(request):
-    cart = Cart(request)    
+    cart = Cart(request)
+    productsstring = ''
+
+    for item in cart:
+        product = item['product']
+        b = "{'id': '%s', 'title': '%s', 'price': '%s', 'quantity': '%s', 'total_price': '%s'}," % (product.id, product.title, product.price, item['quantity'], item['total_price'])
+
+        productsstring = productsstring + b    
 
     context = {
         'cart': cart,
+        'productsstring': productsstring
     }
 
     return render(request, 'cart.html', context)
