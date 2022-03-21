32d0fc879ffade8addea4d5dda6dd4ef7f2749c6
caesar699 <caesar699@gmail.com>
2022-03-21 08:08:15 +0600

    Create new orders and clear cart

--- e_market/apps/cart/cart.py
+++ e_market/apps/cart/cart.py
@@ -56,6 +56,10 @@ class Cart(object):
         self.session[settings.CART_SESSION_ID] = self.cart
         self.session.modified = True
     
+    def clear(self):
+        del self.session[settings.CART_SESSION_ID]
+        self.session.modified = True
+    
     def get_total_length(self):
         return sum(int(item['quantity']) for item in self.cart.values())
 
