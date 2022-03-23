4c381ab00642b1903a70105aa8d3e913a4a7e77a
caesar699 <caesar699@gmail.com>
2022-03-23 06:29:44 +0600

    Install app coupon

--- e_market/e_market/settings.py
+++ e_market/e_market/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.cart',
+    'apps.coupon',
     'apps.core',
     'apps.order',
     'apps.store',    
