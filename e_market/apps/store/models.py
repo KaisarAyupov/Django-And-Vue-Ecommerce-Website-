034d541c35e7f145806474e94737884c51660f37
caesar699 <caesar699@gmail.com>
2022-03-24 09:26:56 +0600

    add multiple images

--- e_market/apps/store/models.py
+++ e_market/apps/store/models.py
@@ -3,14 +3,15 @@ from django.core.files import File
 from django.db import models
 from PIL import Image
 
+
 class Category(models.Model):
-    title= models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
     ordering = models.IntegerField(default=0)
 
     class Meta:
         verbose_name_plural = 'Categories'
-        ordering =('ordering',)
+        ordering = ('ordering',)
 
     def __str__(self):
         return self.title
@@ -18,9 +19,11 @@ class Category(models.Model):
     def get_absolute_url(self):
         return '/%s/' % (self.slug)
 
+
 class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-    title= models.CharField(max_length=255)
+    category = models.ForeignKey(
+        Category, related_name='products', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.FloatField()
@@ -28,10 +31,10 @@ class Product(models.Model):
 
     image = models.ImageField(upload_to='uploads/', blank=True, null=True)
     thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
-    date_added =models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering =('-date_added',)
+        ordering = ('-date_added',)
 
     def __str__(self):
         return self.title
@@ -42,6 +45,31 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return '/%s/%s/' % (self.category.slug, self.slug)
+
+    def make_thumbnail(self, image, size=(300, 200)):
+        img = Image.open(image)
+        img.convert('RGB')
+        img.thumbnail(size)
+
+        thumb_io = BytesIO()
+        img.save(thumb_io, 'JPEG', quality=85)
+
+        thumbnail = File(thumb_io, name=image.name)
+
+        return thumbnail
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(
+        Product, related_name='images', on_delete=models.CASCADE)
+
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.thumbnail = self.make_thumbnail(self.image)
+
+        super().save(*args, **kwargs)
 
     def make_thumbnail(self, image, size=(300, 200)):
         img = Image.open(image)
--- e_market/apps/store/views.py
+++ e_market/apps/store/views.py
@@ -6,13 +6,15 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
 from .models import Product, Category
 
+
 def search(request):
     query = request.GET.get('query')
     instock = request.GET.get('instock')
     price_from = request.GET.get('price_from', 0)
     price_to = request.GET.get('price_to', 100000)
     sorting = request.GET.get('sorting', '-date_added')
-    products = Product.objects.filter(Q(title__icontains=query) | Q(description__icontains=query)).filter(price__gte=price_from).filter(price__lte=price_to)
+    products = Product.objects.filter(Q(title__icontains=query) | Q(
+        description__icontains=query)).filter(price__gte=price_from).filter(price__lte=price_to)
 
     if instock:
         products = products.filter(num_available__gte=1)
@@ -32,8 +34,16 @@ def search(request):
 def product_detail(request, category_slug, slug):
     product = get_object_or_404(Product, slug=slug)
 
+    imagesstring = "{'thumbnail': '%s', 'image': '%s'}," % (product.thumbnail.url, product.image.url)
+
+    for image in product.images.all():
+        imagesstring = imagesstring + \
+            ("{'thumbnail': '%s', 'image': '%s'}," % (
+                image.thumbnail.url, image.image.url))
+
     context = {
-        'product': product
+        'product': product,
+        'imagesstring': imagesstring
     }
 
     return render(request, 'product_detail.html', context)
