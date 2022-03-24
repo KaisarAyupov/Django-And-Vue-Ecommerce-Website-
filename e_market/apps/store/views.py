ff22f384a8307fa143d65dcb8086c19d9fa9a32d
caesar699 <caesar699@gmail.com>
2022-03-24 17:27:11 +0600

    Make the product list an included file

--- e_market/apps/store/views.py
+++ e_market/apps/store/views.py
@@ -15,8 +15,7 @@ def search(request):
     price_from = request.GET.get('price_from', 0)
     price_to = request.GET.get('price_to', 100000)
     sorting = request.GET.get('sorting', '-date_added')
-    products = Product.objects.filter(Q(title__icontains=query) | Q(
-        description__icontains=query)).filter(price__gte=price_from).filter(price__lte=price_to)
+    products = Product.objects.filter(Q(title__icontains=query) | Q(description__icontains=query)).filter(price__gte=price_from).filter(price__lte=price_to)
 
     if instock:
         products = products.filter(num_available__gte=1)
@@ -36,7 +35,13 @@ def search(request):
 def product_detail(request, category_slug, slug):
     product = get_object_or_404(Product, slug=slug)
 
+    related_products = list(product.category.products.filter(parent=None).exclude(id=product.id))
     
+    if len(related_products) >= 3:
+        related_products = random.sample(related_products, 3)
+
+    if product.parent:
+        return redirect('product_detail', category_slug=category_slug, slug=product.parent.slug)
 
     imagesstring = "{'thumbnail': '%s', 'image': '%s'}," % (product.thumbnail.url, product.image.url)
 
@@ -54,7 +59,8 @@ def product_detail(request, category_slug, slug):
 
     context = {
         'product': product,
-        'imagesstring': imagesstring
+        'imagesstring': imagesstring,
+        'related_products': related_products
     }
 
     return render(request, 'product_detail.html', context)
