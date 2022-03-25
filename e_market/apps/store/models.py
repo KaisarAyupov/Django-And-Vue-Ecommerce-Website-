36cfe7202e8ab1cfcd7ad14bf98d5e228ed1ea79
caesar699 <caesar699@gmail.com>
2022-03-25 10:40:31 +0600

    Add sub categories to the project

--- e_market/apps/store/models.py
+++ e_market/apps/store/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 
 
 class Category(models.Model):
+    parent = models.ForeignKey('self', related_name='children', on_delete=models.CASCADE, blank=True, null=True)
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
     ordering = models.IntegerField(default=0)
