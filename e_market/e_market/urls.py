dd1d30b45b1e1d436c38fdb71c05e2b71ab52aaa
caesar699 <caesar699@gmail.com>
2022-03-25 15:58:24 +0600

    urls

--- e_market/e_market/urls.py
+++ e_market/e_market/urls.py
@@ -8,15 +8,16 @@ from django.contrib.auth import views
 from apps.cart.webhook import webhook
 from apps.cart.views import cart_detail, success
 from apps.core.views import frontpage, contact, about
-
+from apps.order.views import admin_order_pdf
 from apps.store.views import product_detail, category_detail, search
 from apps.userprofile.views import signup, myaccount
-from apps.order.views import admin_order_pdf
+
 from apps.newsletter.api import api_add_subscriber
 from apps.coupon.api import api_can_use
 from apps.store.api import api_add_to_cart, api_remove_from_cart, create_checkout_session, validate_payment
 
 from .sitemaps import StaticViewSitemap, CategorySitemap, ProductSitemap
+
 sitemaps = {'static': StaticViewSitemap, 'product': ProductSitemap, 'category': CategorySitemap}
 
 urlpatterns = [
@@ -24,13 +25,14 @@ urlpatterns = [
     path('search/', search, name='search'),
     path('cart/', cart_detail, name='cart'),
     path('hooks/', webhook, name='webhook'),
-    path('cart/success', success, name='success'),
+    path('cart/success/', success, name='success'),
     path('contact/', contact, name='contact'),
     path('about/', about, name='about'),
     path('admin/', admin.site.urls),
     path('admin/admin_order_pdf/<int:order_id>/', admin_order_pdf, name='admin_order_pdf'),
 
-     # Auth
+    # Auth
+
     path('myaccount/', myaccount, name='myaccount'),
     path('signup/', signup, name='signup'),
     path('login/', views.LoginView.as_view(template_name='login.html'), name='login'),
@@ -39,16 +41,16 @@ urlpatterns = [
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 
     # API
+
     path('api/can_use/', api_can_use, name='api_can_use'),
     path('api/create_checkout_session/', create_checkout_session, name='create_checkout_session'),
     path('api/validate_payment/', validate_payment, name='validate_payment'),
-    path('api/api_add_to_cart/', api_add_to_cart, name='api_add_to_cart'),
+    path('api/add_to_cart/', api_add_to_cart, name='api_add_to_cart'),
     path('api/remove_from_cart/', api_remove_from_cart, name='api_remove_from_cart'),
     path('api/add_subscriber/', api_add_subscriber, name='api_add_subscriber'),
+
     # Store
 
-
-    path('<slug:category_slug>/<slug:slug>/', product_detail, name='product_detail' ),
-    path('<slug:slug>/', category_detail, name='category_detail' ),
-    
+    path('<slug:category_slug>/<slug:slug>/', product_detail, name='product_detail'),
+    path('<slug:slug>/', category_detail, name='category_detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
